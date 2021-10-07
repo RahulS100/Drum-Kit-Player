b729ac4a624a5ef704e574e6a555f97e519942d8
rahul <rahulsaharan554@gmail.com>
2021-10-08 02:14:55 +0530

    minor bugs fixed and minor changes done

--- app.js
+++ app.js
@@ -9,17 +9,12 @@ for(let i=0; i<btn_len; i++) {
     drums[i].addEventListener('click', ()=>{
             playSound(drums[i].innerHTML);
 
-            ///animation function call
-            animation_btn(drums[i].innerHTML);
     });
 }//click event listner
 
 
 document.addEventListener('keypress', (evt) => {
     playSound(evt.key);
-
-    //animation function call
-    animation_btn(evt.key);
 
 }); ///////////keypress event listner
 
@@ -37,37 +32,62 @@ function animation_btn(key) {
 ////////////////////////function for playing the drum audio//////////////////////////
 function playSound(match) {
     if(match === 'a') {
+        //animation function call
+        animation_btn(match);
+
         let drum_audio = new Audio('/sounds/crash.mp3');
         drum_audio.play();
     } 
     
     else if(match === 's') {
+        //animation function call
+        animation_btn(match);
+
         let drum_audio = new Audio('/sounds/kick-bass.mp3');
         drum_audio.play();
     }
     
     else if(match === 'd') {
+        //animation function call
+        animation_btn(match);
+
         let drum_audio = new Audio('/sounds/snare.mp3');
         drum_audio.play();
     }
     
     else if(match === 'f') {
+        //animation function call
+        animation_btn(match);
+
         let drum_audio = new Audio('/sounds/tom-1.mp3');
         drum_audio.play();
     }
     
     else if(match === 'g') {
+        //animation function call
+        animation_btn(match);
+
         let drum_audio = new Audio('/sounds/tom-2.mp3');
         drum_audio.play();
     }
     
     else if(match === 'h') {
+        //animation function call
+        animation_btn(match);
+
         let drum_audio = new Audio('/sounds/tom-3.mp3');
         drum_audio.play();
     }
     
     else if(match === 'j') {
+        //animation function call
+        animation_btn(match);
+
         let drum_audio = new Audio('/sounds/tom-4.mp3');
         drum_audio.play();
+    }
+
+    else {
+        console.log("Not any operation with this key" + match);
     }
 } 
